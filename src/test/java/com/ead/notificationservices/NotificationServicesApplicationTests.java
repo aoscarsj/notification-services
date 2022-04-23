46b33566e92359371b5f1bdc4ff11a04f7623c5b
Artur Oscar <Arturoscar3@gmail.com>
2022-04-23 16:37:27 -0300

    First commit

---
+++ src/test/java/com/ead/notificationservices/NotificationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.notificationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
